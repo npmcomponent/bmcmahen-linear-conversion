63c5b32c0eedcc02a12d33c3e830b264e4498d9f
Ben McMahen <ben.mcmahen@gmail.com>
2013-05-09 11:44:02 -0600

    initial commit

---
+++ Readme.md
@@ -0,0 +1,21 @@
+
+# scale-bilinear
+
+	The d3 bilinear scale (with alterations), extracted for use in non-d3 projects.
+
+## Installation
+
+    $ component install bmcmahen/scale-bilinear
+
+## API
+
+	// Import the module
+	var scaleBilinear = require('bmcmahen-scale-bilinear');
+
+	// Create your scale, with a domain and range
+  var scale = scaleBilinear([0, 10], [0, 100]);
+  alert(scale(1)); // alerts 10
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,25 @@
+// Full credit goes to d3 (https://github.com/mbostock/d3?source=cc) which this is
+// basically extracted from...
+
+module.exports = function(domain, range){
+	var uninter = uninterpolateNumber(domain[0], domain[1])
+		, inter = interpolateNumber(range[0], range[1]);
+
+	return function(x){
+		return uninter(inter(x));
+	};
+};
+
+function interpolateNumber(a, b){
+	b -= a = +a;
+	return function(t){
+		return a + b * t;
+	};
+}
+
+function uninterpolateNumber(a, b){
+	b = b - (a = +a) ? 1 / (b - a) : 0;
+	return function(x) {
+    return (x - a) * b;
+  };
+}
